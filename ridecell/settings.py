d23445c06396c35267a09b61406f8cb1e12da2ad
Scott Young <styoung89@gmail.com>
2017-04-13 21:24:42 -0700

    postgres

--- ridecell/settings.py
+++ ridecell/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 
 if os.environ.get('local') is None:
-    DATABASES['default'] = dj_database_url.config(default='postgres://user:pass@localhost/dbname')
+    DATABASES['default'] = dj_database_url.config()
     DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 
